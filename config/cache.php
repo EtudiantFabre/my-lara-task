37e2234143e298893ad7e9b9467a30edd2020101
EtudiantFabre <fabricetoyi87@gmail.com>
2025-10-14 16:17:47 +0000

    MAJ

--- config/cache.php
+++ config/cache.php
@@ -48,8 +48,8 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
-            'lock_path' => storage_path('framework/cache/data'),
+            'path' => storage_path('tmp/framework/cache/data'),
+            'lock_path' => storage_path('tmp/framework/cache/data'),
         ],
 
         'memcached' => [
